b7361770f762c70c42d45cdca9925853849f348d
Osku Sirpoma <Osku Sirpoma>
2017-04-13 16:23:19 +0300

    Testing new profit algorithm

--- src/Game1/VoitonJako.java
+++ src/Game1/VoitonJako.java
@@ -7,91 +7,213 @@ package Game1;
 
 /**
  *
+ *
  * @author s1601396
  */
 public class VoitonJako {
 
     public static double Taso1(int num2, int num5, int num8) {
         if (num2 == num5 && num5 == num8 && num8 == 9) {
-            System.out.println("L1 x5");
             return (5);
-        } else if (num2 == num5 && num5 == num8) {
-            System.out.println("L1 x3");
+        } else if (num2 == num5 && num5 == num8 && num8 == 8) {
+            return (4);
+        } else if (num2 == num5 && num5 == num8 && num8 == 7) {
+            return (4);
+        } else if (num2 == num5 && num5 == num8 && num8 == 6) {
+            return (4);
+        } else if (num2 == num5 && num5 == num8 && num8 == 5) {
+            return (4);
+        } else if (num2 == num5 && num5 == num8 && num8 == 4) {
             return (3);
+        } else if (num2 == num5 && num5 == num8 && num8 == 3) {
+            return (3);
+        } else if (num2 == num5 && num5 == num8 && num8 == 2) {
+            return (3);
+        } else if (num2 == num5 && num5 == num8 && num8 == 1) {
+            return (3);
+        } else if (num2 == num5 && num5 == 9) {
+            return (3);
+        } else if (num2 == num5 && num5 == 8) {
+            return (1.5);
+        } else if (num2 == num5 && num5 == 7) {
+            return (1.5);
+        } else if (num2 == num5 && num5 == 6) {
+            return (1.5);
         } else if (num2 == num5 && num5 == 5) {
-            System.out.println("L1 x3");
-            return (3);
-        } else if (num2 == num5) {
-            System.out.println("L1 x2");
-            return (2);
+            return (1.5);
+        } else if (num2 == num5 && num5 == 4) {
+            return (0.75);
+        } else if (num2 == num5 && num5 == 3) {
+            return (0.75);
+        } else if (num2 == num5 && num5 == 2) {
+            return (0.75);
+        } else if (num2 == num5 && num5 == 1) {
+            return (0.75);
         }
+
         return 0;
     }
 
     public static double Taso2(int num3, int num6, int num9) {
         if (num3 == num6 && num6 == num9 && num9 == 9) {
-            System.out.println("L2 x5");
             return (5);
-        } else if (num3 == num6 && num6 == num9) {
-            System.out.println("L2 x3");
+        } else if (num3 == num6 && num6 == num9 && num9 == 8) {
+            return (4);
+        } else if (num3 == num6 && num6 == num9 && num9 == 7) {
+            return (4);
+        } else if (num3 == num6 && num6 == num9 && num9 == 6) {
+            return (4);
+        } else if (num3 == num6 && num6 == num9 && num9 == 5) {
+            return (4);
+        } else if (num3 == num6 && num6 == num9 && num9 == 4) {
             return (3);
+        } else if (num3 == num6 && num6 == num9 && num9 == 3) {
+            return (3);
+        } else if (num3 == num6 && num6 == num9 && num9 == 2) {
+            return (3);
+        } else if (num3 == num6 && num6 == num9 && num9 == 1) {
+            return (3);
+        } else if (num3 == num6 && num6 == 9) {
+            return (3);
+        } else if (num3 == num6 && num6 == 8) {
+            return (1.5);
+        } else if (num3 == num6 && num6 == 7) {
+            return (1.5);
+        } else if (num3 == num6 && num6 == 6) {
+            return (1.5);
         } else if (num3 == num6 && num6 == 5) {
-            System.out.println("L2 x3");
-            return (3);
-        } else if (num3 == num6) {
-            System.out.println("L2 x2");
-            return (2);
+            return (1.5);
+        } else if (num3 == num6 && num6 == 4) {
+            return (0.75);
+        } else if (num3 == num6 && num6 == 3) {
+            return (0.75);
+        } else if (num3 == num6 && num6 == 2) {
+            return (0.75);
+        } else if (num3 == num6 && num6 == 1) {
+            return (0.75);
         }
         return 0;
     }
 
     public static double Taso3(int num1, int num4, int num7) {
         if (num1 == num4 && num4 == num7 && num7 == 9) {
-            System.out.println("L3 x5");
             return (5);
-        } else if (num1 == num4 && num4 == num7) {
-            System.out.println("L3 x3");
+        } else if (num1 == num4 && num4 == num7 && num7 == 8) {
+            return (4);
+        } else if (num1 == num4 && num4 == num7 && num7 == 7) {
+            return (4);
+        } else if (num1 == num4 && num4 == num7 && num7 == 6) {
+            return (4);
+        } else if (num1 == num4 && num4 == num7 && num7 == 5) {
+            return (4);
+        } else if (num1 == num4 && num4 == num7 && num7 == 4) {
             return (3);
+        } else if (num1 == num4 && num4 == num7 && num7 == 3) {
+            return (3);
+        } else if (num1 == num4 && num4 == num7 && num7 == 2) {
+            return (3);
+        } else if (num1 == num4 && num4 == num7 && num7 == 1) {
+            return (3);
+        } else if (num1 == num4 && num4 == 9) {
+            return (3);
+        } else if (num1 == num4 && num4 == 8) {
+            return (1.5);
+        } else if (num1 == num4 && num4 == 7) {
+            return (1.5);
+        } else if (num1 == num4 && num4 == 6) {
+            return (1.5);
         } else if (num1 == num4 && num4 == 5) {
-            System.out.println("L3 x3");
-            return (3);
-        } else if (num1 == num4) {
-            System.out.println("L3 x2");
-            return (2);
+            return (1.5);
+        } else if (num1 == num4 && num4 == 4) {
+            return (0.75);
+        } else if (num1 == num4 && num4 == 3) {
+            return (0.75);
+        } else if (num1 == num4 && num4 == 2) {
+            return (0.75);
+        } else if (num1 == num4 && num4 == 1) {
+            return (0.75);
         }
         return 0;
     }
 
     public static double Taso4(int num1, int num5, int num9) {
         if (num1 == num5 && num5 == num9 && num9 == 9) {
-            System.out.println("L4 x5");
             return (5);
-        } else if (num1 == num5 && num5 == num9) {
-            System.out.println("L4 x3");
+        } else if (num1 == num5 && num5 == num9 && num9 == 8) {
+            return (4);
+        } else if (num1 == num5 && num5 == num9 && num9 == 7) {
+            return (4);
+        } else if (num1 == num5 && num5 == num9 && num9 == 6) {
+            return (4);
+        } else if (num1 == num5 && num5 == num9 && num9 == 5) {
+            return (4);
+        } else if (num1 == num5 && num5 == num9 && num9 == 4) {
             return (3);
+        } else if (num1 == num5 && num5 == num9 && num9 == 3) {
+            return (3);
+        } else if (num1 == num5 && num5 == num9 && num9 == 2) {
+            return (3);
+        } else if (num1 == num5 && num5 == num9 && num9 == 1) {
+            return (3);
+        } else if (num1 == num5 && num5 == 9) {
+            return (3);
+        } else if (num1 == num5 && num5 == 8) {
+            return (1.5);
+        } else if (num1 == num5 && num5 == 7) {
+            return (1.5);
+        } else if (num1 == num5 && num5 == 6) {
+            return (1.5);
         } else if (num1 == num5 && num5 == 5) {
-            System.out.println("L4 x3");
-            return (3);
-        } else if (num1 == num5) {
-            System.out.println("L4 x2");
-            return (2);
+            return (1.5);
+        } else if (num1 == num5 && num5 == 4) {
+            return (0.75);
+        } else if (num1 == num5 && num5 == 3) {
+            return (0.75);
+        } else if (num1 == num5 && num5 == 2) {
+            return (0.75);
+        } else if (num1 == num5 && num5 == 1) {
+            return (0.75);
         }
         return 0;
     }
 
     public static double Taso5(int num3, int num5, int num7) {
         if (num3 == num5 && num5 == num7 && num7 == 9) {
-            System.out.println("L5 x5");
             return (5);
-        } else if (num3 == num5 && num5 == num7) {
-            System.out.println("L5 x3");
+        } else if (num3 == num5 && num5 == num7 && num7 == 8) {
+            return (4);
+        } else if (num3 == num5 && num5 == num7 && num7 == 7) {
+            return (4);
+        } else if (num3 == num5 && num5 == num7 && num7 == 6) {
+            return (4);
+        } else if (num3 == num5 && num5 == num7 && num7 == 5) {
+            return (4);
+        } else if (num3 == num5 && num5 == num7 && num7 == 4) {
             return (3);
+        } else if (num3 == num5 && num5 == num7 && num7 == 3) {
+            return (3);
+        } else if (num3 == num5 && num5 == num7 && num7 == 2) {
+            return (3);
+        } else if (num3 == num5 && num5 == num7 && num7 == 1) {
+            return (3);
+        } else if (num3 == num5 && num5 == 9) {
+            return (3);
+        } else if (num3 == num5 && num5 == 8) {
+            return (1.5);
+        } else if (num3 == num5 && num5 == 7) {
+            return (1.5);
+        } else if (num3 == num5 && num5 == 6) {
+            return (1.5);
         } else if (num3 == num5 && num5 == 5) {
-            System.out.println("L5 x3");
-            return (3);
-        } else if (num3 == num5) {
-            System.out.println("L5 x2");
-            return (2);
+            return (1.5);
+        } else if (num3 == num5 && num5 == 4) {
+            return (0.75);
+        } else if (num3 == num5 && num5 == 3) {
+            return (0.75);
+        } else if (num3 == num5 && num5 == 2) {
+            return (0.75);
+        } else if (num3 == num5 && num5 == 1) {
+            return (0.75);
         }
         return 0;
     }
