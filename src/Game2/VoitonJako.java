3c060e08c581768fd53ac011a634dfc5c54b031c
Sirpoma Osku <s1601396@student.businesscollege.fi>
2017-04-21 10:41:34 +0300

    Added profit class for game2

---
+++ src/Game2/VoitonJako.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game2;
+
+/**
+ *
+ * @author s1601396
+ */
+public class VoitonJako {
+    
+    public static double Taso1(int num2, int num5, int num8) {
+        return 0;
+    }
+}
