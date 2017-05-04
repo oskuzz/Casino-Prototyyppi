70c4633493f98ce567511a5801e98c54ddac6f6f
Sirpoma Osku <s1601396@student.businesscollege.fi>
2017-05-04 15:14:44 +0300

    Added button to ProfileChange

--- src/Casino/CasinoFirstPage.java
+++ src/Casino/CasinoFirstPage.java
@@ -360,6 +360,7 @@ public final class CasinoFirstPage extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         this.setVisible(false);
+        new ProfileChange().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
